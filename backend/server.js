50b1bdbaef3613e96243a7a58f6cc0401afd7af7
ShaKk0722 <duythaithanh2004@gmail.com>
2025-04-30 19:37:04 +0700

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
+import path from "path";
 import express from "express";  
+
 import dotenv from "dotenv";  
 dotenv.config();
 
@@ -8,18 +10,18 @@ import connectToMongoDB from "./db/connectToMongoDB.js";
 import cookieParser from "cookie-parser";
 import { app, server } from "./socket/socket.js";
 
-
-const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve(); // Get the current directory path
+const PORT = process.env.PORT || 5000;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/mydatabase";
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
 
 app.use(express.json()); // Middleware to parse JSON bodies
 app.use(cookieParser()); // Middleware to parse cookies
 initRoutes(app);
-
+app.use(express.static(path.join(__dirname, "/frontend/dist"))); // Serve static files from the public directory
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html")); // Serve the index.html file for all other routes
+});
 
 server.listen(PORT, () => {
   connectToMongoDB();
